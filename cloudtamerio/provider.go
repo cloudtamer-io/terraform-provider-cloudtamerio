6b61ea6049c267a4199e42edd9d1273ba771a4c9
Joseph Spurrier <jspurrier@cloudtamer.io>
2021-05-06 15:19:32 -0400

    Change provider to "ou". Regenerate docs

--- cloudtamerio/provider.go
+++ cloudtamerio/provider.go
@@ -42,7 +42,7 @@ func Provider() *schema.Provider {
 			"cloudtamerio_compliance_standard":         resourceComplianceStandard(),
 			"cloudtamerio_ou_cloud_access_role":        resourceOUCloudAccessRole(),
 			"cloudtamerio_project_cloud_access_role":   resourceProjectCloudAccessRole(),
-			"cloudtamerio_organizational_unit":         resourceOU(),
+			"cloudtamerio_ou":                          resourceOU(),
 		},
 		DataSourcesMap: map[string]*schema.Resource{
 			"cloudtamerio_aws_cloudformation_template": dataSourceAwsCloudformationTemplate(),
@@ -51,7 +51,7 @@ func Provider() *schema.Provider {
 			"cloudtamerio_cloud_rule":                  dataSourceCloudRule(),
 			"cloudtamerio_compliance_check":            dataSourceComplianceCheck(),
 			"cloudtamerio_compliance_standard":         dataSourceComplianceStandard(),
-			"cloudtamerio_organizational_unit":         dataSourceOU(),
+			"cloudtamerio_ou":                          dataSourceOU(),
 		},
 		ConfigureContextFunc: providerConfigure,
 	}
